fccd43014ddd897a4048801f7eed8955414110dc
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-12-10 01:35:10 +0100

    Update layout and styling.

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -33,7 +33,7 @@ export class UserRegistrationFormComponent implements OnInit {
       // Logic for a successful user registration goes here
       this.dialogRef.close(); // This will close the modal on success!
       console.log(result);
-      this.snackBar.open(result, 'OK', {
+      this.snackBar.open('Signed up successfully.', 'OK', {
         duration: 2000
       });
     }, (result) => {
